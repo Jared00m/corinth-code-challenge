aa741b0ff535d77883fe970bbbe69ce76d05cd0a
Jared Mitcham <jaredmitcham@gmail.com>
2022-01-29 18:24:45 -0500

    Minor style fix.

--- corinth-code-backend/src/routes/swapi/swapi.routes.ts
+++ corinth-code-backend/src/routes/swapi/swapi.routes.ts
@@ -3,9 +3,7 @@ import * as swapiControllers from '../../controllers/swapi/swapi.contoller';
 
 const router = express.Router();
 
-
 router.get('/people', swapiControllers.getPeople);
 router.get('/person', swapiControllers.getPerson);
-
 
 export default router;
